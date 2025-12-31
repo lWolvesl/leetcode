2797ee2b178840b15a12d808e93cd1cf25d709e5
wolves <yangwolves@foxmail.com>
2026-01-01 04:50:39 +0800

    routine

---
+++ 26/01/go/66.go
@@ -0,0 +1,16 @@
+package A
+
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] == 9 {
+			digits[i] = 0
+			if i == 0 {
+				digits = append([]int{1}, digits...)
+			}
+			continue
+		}
+		digits[i]++
+		break
+	}
+	return digits
+}
